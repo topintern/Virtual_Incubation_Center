37bfd87cae8a916df16ea8108c407b3db34307dd
topintern <neellohit69@gmail.com>
2022-11-27 04:56:13 -0600

    fix: about us page

---
+++ aboutus.php
@@ -0,0 +1,226 @@
+<html>
+<head>
+    
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    
+    <title>Virtual Incubation Center</title>
+    <link rel="icon" href="./img/logo.png" type="image/icon type">
+</head>
+<body>
+    <nav class="navbar navbar-dark navbar-expand-sm fixed-top">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Navbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand mr-auto" href="#"><img src="img/logo.png" height="30" width="41" alt="logo"></a>
+            <div class="collapse navbar-collapse" id="Navbar">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><a class="nav-link" href="./Main_home.php"><span class="fa fa-home fa-lg"></span> Home</a></li>
+                    <li class="nav-item active"><a class="nav-link" href="#"><span class="fa fa-info fa-lg"></span> About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./public_projects.php"><span class="fa fa-list fa-lg"></span> Our Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="./contactus.php"><span class="fa fa-address-card fa-lg"></span> Contact Us</a></li>
+                </ul>
+                <span class="navbar-text">
+                    <a class="nav-link" href="./login.php" target="_blank">
+                        <span class="fa fa-sign-in"></span> Login
+                    </a>
+                </span>
+            </div>
+        </div>
+    </nav>
+
+
+    <header class="jumbotron">
+        <div class="container">
+            <div class="row row-header">
+                <div class="col-12 col-sm-6">
+                    <h1>Virtual Incubation Center</h1>
+                    <h5>Are you ready for your business to launch in the market?</h5>
+                    <p>Let a group of industry mentors, angel and venture capitalists and corporate honchos, join your team!</p>
+                </div>
+                
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <!--<div class="row row-header">
+            <ol class="col-12 breadcrumb">
+                <li class="breadcrumb-item"><a href="./index.html">Home</a></li>
+                <li class="breadcrumb-item active">About Us</li>
+            </ol>
+            <div class="col-12">
+               <h3>About Us</h3>
+               <hr>
+            </div>
+        </div>-->
+
+        <div class="row row-content">
+            <div class="col-12 col-sm-6">
+                <h2>Our History</h2>
+                <p>Our virtual incubation program supports close to 400 startups every year. It is designed to help early stage to mid stage startups looking for a kick-start and connects. The program creates the optimal environment for such startups to scale up by providing support in the form of a Startup Kit and connecting the company with our widespread network of evaluators and mentors, who are also investors, accelerators, and industry veterans</p>
+                <p>As one of the largest of its kind, the Virtual Program receives a significant number of applications. The selection process entails two-rounds of screening process by the regional leads and our committee members consisting of external stakeholders. To expedite the selection process and reduce the waiting time for startups, we now enrol startups in the virtual incubate program every month.</p>
+            </div>
+            <div class="col-12 col-sm-6">
+                <div class="card">
+                    <h3 class="card-header bg-success text-white">Facts At a Glance</h3>
+                    <div class="card-body">
+                        <dl class="row">
+                            <dt class="col-6">Started</dt>
+                            <dd class="col-6"> 14 Feb. 2012</dd>
+                            <dt class="col-6">Major Investor</dt>
+                            <dd class="col-6">Elon Musk</dd>
+                            <dt class="col-6">Last Year's Funding</dt>
+                            <dd class="col-6">$15M</dd>
+                            
+                        </dl>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="card card-body bg-light">
+                    <blockquote class="blockquote">
+                        <p class="mb-0">NEXT mentors provide unbiased, conflict-free, practical, and actionable advice on a long-term basis. MIT’s strict code of ethics ensures a safe, trusting environment for our members.</p>
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row row-content">
+            <div class="co1-12">
+                <h2>Our Mentors</h2>
+                <div id="accordion">
+                    <div class="card">
+                        <div class="card-header" role="tab" id="peterhead">
+                            <h3 class="mb-0">
+                                <a class="collapsed" data-toggle="collapse" data-target="#peter">
+                                    Mr. Johnson Thomas
+                                </a>
+                            </h3>
+                        </div>
+                        
+                        <div class="collapse" id="peter" data-parent="#accordion">
+                            <div class="row">
+                                <div class="col-6 offset-1 col-sm-6">
+                                    <img class="d-block img-fluid" src="img/jt.jpg" alt="Johnson Thomas">
+                                </div>
+                                <div class="col-5 col-sm-5">
+                                    <br>
+                                    <i class="fa fa-phone fa-lg"></i>: 918-594-8103<br>
+                                    <br>
+                                    <i class="fa fa-envelope fa-lg"></i>: <a href=
+                                    "https://mail.google.com/mail/?view=cm&fs=1&to=neilgogte@kmit.in&su=SUBJECT&body=BODY" target="_blank">jpt@cs.okstate.edu</a><br>
+                                    <br>
+                                    <a class="btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/johnson-thomas-321a1113" target="_blank"><i class="fa fa-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <p class="d-none d-sm-block">Dr. Thomas received his PhD from the University of Reading, England.  His research interests include security and privacy, cognitive computing, human health, and big data.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </div>
+       <div class="row row-content">
+           <div class="col-12 col-sm-9">
+               <h2>Facts &amp; Figures</h2>
+               <div class="table-responsive">
+                   <table class="table table-striped">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>&nbsp;</th>
+                                <th>2013</th>
+                                <th>2014</th>
+                                <th>2015</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>Employees</th>
+                                <td>15</td>
+                                <td>30</td>
+                                <td>40</td>
+                            </tr>
+                            <tr>
+                                <th>Projects Funded</th>
+                                <td>15000</td>
+                                <td>45000</td>
+                                <td>100,000</td>
+                            </tr>
+                            <tr>
+                                <th>No of Investors</th>
+                                <td>30</td>
+                                <td>200</td>
+                                <td>456</td>
+                            </tr>
+                            <tr>
+                                <th>Annual Funding</th>
+                                <td>$500K</td>
+                                <td>$1M</td>
+                                <td>3M</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-12 col-sm-3">
+            </div>
+        </div> 
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">             
+                <div class="col-4 offset-1 col-sm-2">
+                    <!-- <h5>Links</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="./Main_home.php">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="./contactus.php">Contact</a></li>
+                    </ul> -->
+                </div>
+                <div class="col-7 col-sm-5">
+                    <h5>Our Address</h5>
+                    <address>
+		              Nancy Randolph Davis Building<br>
+		              122 N Monroe St, Stillwater, Oklahoma State University<br>
+                      OK 74075<br>
+		              <i class="fa fa-phone fa-lg"></i>: (475) 291-1455<br>
+		              <i class="fa fa-fax fa-lg"></i>: (475) 291-1455<br>
+		              <i class="fa fa-envelope fa-lg"></i>: <a href="mailto:vic.info@gmail.com">vic.info@gmail.com</a>
+		           </address>
+                </div>
+                <div class="col-12 col-sm-4 align-self-center">
+                    <div class="text-center">
+                        <a class="btn btn-social-icon btn-google" href="http://google.com/+" target="_blank"><i class="fa fa-google-plus fa-lg"></i></a>
+                        <a class="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id=" target="_blank"><i class="fa fa-facebook fa-lg"></i></a>
+                        <a class="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/" target="_blank"><i class="fa fa-linkedin fa-lg"></i></a>
+                        <a class="btn btn-social-icon btn-twitter" href="http://twitter.com/" target="_blank"><i class="fa fa-twitter fa-lg"></i></a>
+                        <a class="btn btn-social-icon btn-youtube" href="http://youtube.com/" target="_blank"><i class="fa fa-youtube fa-lg"></i></a>
+                        <a class="btn btn-social-icon " href="mailto:" target="_blank"><i class="fa fa-envelope-o fa-lg"></i></a>
+                    </div>
+                </div>
+           </div>
+           <div class="row justify-content-center">             
+                <div class="col-auto">
+                    <p>© Copyright 2021. All rights reserved.</p>
+                </div>
+           </div>
+        </div>
+    </footer>
+
+    <!-- JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
